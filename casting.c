bbb0278db5a2ed42b6925668b13f1f10f0129a3c
xhelp00 <xhelp00@gmail.com>
2023-10-11 16:44:38 +0200

    added sound effects

--- casting.c
+++ casting.c
@@ -6,7 +6,7 @@
 /*   By: phelebra <xhelp00@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 14:04:56 by jbartosi          #+#    #+#             */
-/*   Updated: 2023/10/16 14:55:15 by phelebra         ###   ########.fr       */
+/*   Updated: 2023/10/16 14:58:58 by phelebra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -314,6 +314,8 @@ void	cast_obj(t_box *box)
 							else
 							{
 								box->info.color = extract_color(&box->textures[sprites->data->texture].addr[(box->info.tex_x * 4) + box->textures[sprites->data->texture].line_len * box->info.tex_y + box->textures[sprites->data->texture].line_len * -16]);
+								box->info.sound = 0;
+							}
 						}
 						else
 							box->info.color = 0;
